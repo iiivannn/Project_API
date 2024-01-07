d1212c1da6815b1536fa300907110d52caa60742
ivan <oraionstarr@gmail.com>
2024-01-07 19:56:26 +0800

    separated

--- lib/navigation/nav_bar.dart
+++ lib/navigation/nav_bar.dart
@@ -1,79 +1,103 @@
 // ignore_for_file: library_private_types_in_public_api
 
-import 'package:app_api/screen/home_screen.dart';
+import 'package:app_api/screen/kana.dart';
+import 'package:app_api/screen/setting.dart';
 import 'package:flutter/material.dart';
+import 'package:app_api/screen/home_screen.dart';
 
 class NavBar extends StatefulWidget {
-  const NavBar({super.key});
+  const NavBar({Key? key}) : super(key: key);
 
   @override
   _NavBarState createState() => _NavBarState();
 }
 
 class _NavBarState extends State<NavBar> {
+  late PageController _pageController;
   int _currentIndex = 0;
 
-  final List<Widget> _pages = [
-    const HomePage(),
-    KitKana(),
-    SettingScreen(),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('image/Wallpaper_4.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Scaffold(
-          backgroundColor: Colors.white10,
-          appBar: AppBar(
-            iconTheme: const IconThemeData(color: Colors.red, size: 35),
-            leading: const Icon(Icons.swap_horizontal_circle),
-            centerTitle: true,
-            flexibleSpace: Container(
-                decoration: const BoxDecoration(
+      home: Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.red, size: 35),
+          centerTitle: false, // CenterTitle set to false
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
               color: Color.fromARGB(255, 28, 31, 63),
-            )),
-            title: const Text(
-              'TranslateEASE',
-              style: TextStyle(
+            ),
+          ),
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.swap_horizontal_circle),
+              SizedBox(width: 8), // Adjust the spacing as needed
+              Text(
+                'TranslateEASE',
+                style: TextStyle(
                   fontFamily: 'Gruppo',
                   color: Colors.red,
                   fontWeight: FontWeight.bold,
-                  fontSize: 30),
-            ),
-          ),
-          body: _pages[_currentIndex],
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: const Color.fromARGB(255, 28, 31, 63),
-            currentIndex: _currentIndex,
-            selectedItemColor: Colors.red,
-            unselectedItemColor: Colors.white,
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.menu_book),
-                label: 'KanaKit',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: 'Settings',
+                  fontSize: 30,
+                ),
               ),
             ],
           ),
+        ),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          children: const <Widget>[
+            HomePage(),
+            KitKana(),
+            SettingScreen(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 28, 31, 63),
+          currentIndex: _currentIndex,
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Colors.white,
+          onTap: (index) {
+            _pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book),
+              label: 'KanaKit',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
         ),
       ),
     );
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,8 +3,6 @@
 import 'package:app_api/languages/languages.dart';
 import 'package:app_api/translation/translation_api.dart';
 import 'package:flutter/material.dart';
-import 'package:card_swiper/card_swiper.dart';
-import 'package:kana_kit/kana_kit.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -50,16 +48,18 @@ class _HomePageState extends State<HomePage> {
                     txt = value;
                   });
                 },
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   labelText: 'Enter Text to Translate',
                   border: OutlineInputBorder(
-                    borderSide: BorderSide(
+                    borderSide: const BorderSide(
                       color: Color.fromARGB(255, 108, 155, 181),
                       width: 2.0,
                     ),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  labelStyle: TextStyle(fontFamily: 'Alata', fontSize: 20.0),
-                  contentPadding: EdgeInsets.symmetric(
+                  labelStyle:
+                      const TextStyle(fontFamily: 'Alata', fontSize: 20.0),
+                  contentPadding: const EdgeInsets.symmetric(
                     horizontal: 20.0,
                     vertical: 20.0,
                   ), // Set the desired padding
@@ -83,7 +83,7 @@ class _HomePageState extends State<HomePage> {
                     color: const Color.fromARGB(255, 70, 90, 99),
                     width: 2.0,
                   ),
-                  borderRadius: BorderRadius.circular(5.0),
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -136,206 +136,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-class KanaKitScreen extends StatelessWidget {
-  const KanaKitScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dito yung KanaKit', style: TextStyle(fontSize: 20)),
-    );
-  }
-}
-
-class KitKana extends StatefulWidget {
-  @override
-  _KitKanaState createState() => _KitKanaState();
-}
-
-class _KitKanaState extends State<KitKana> {
-  final KanaKit kanaKit = const KanaKit();
-  final TextEditingController _textController = TextEditingController();
-  String _result = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('image/Wallpaper_5.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Scaffold(
-          backgroundColor: Colors.white10,
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              'kanaKIT',
-              style: TextStyle(
-                fontFamily: 'OverlockSC',
-                color: Colors.deepOrange,
-                fontSize: 45,
-              ),
-            ),
-          ),
-
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: _textController,
-                  decoration: const InputDecoration(labelText: 'Enter text'),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _result = kanaKit.toRomaji(_textController.text);
-                    });
-                  },
-                  child: const Text(
-                    'Convert to Romaji',
-                    style: TextStyle(fontFamily: 'OverlockSC', fontSize: 15),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  _result,
-                  style: const TextStyle(
-                      fontSize: 30,
-                      fontFamily: 'OverlockSC',
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-
-          //Center(
-          //child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          //children: [
-          //Text(
-          //'Romaji: ${kanaKit.toRomaji('こんにちは')}',
-          //style:
-          //const TextStyle(fontSize: 35, fontFamily: 'OverlockSC'),
-          //),
-          //],
-          //),
-          //),
-        ),
-      ),
-    );
-  }
-}
-
-class SettingScreen extends StatefulWidget {
-  @override
-  _SettingScreen createState() => _SettingScreen();
-}
-
-class _SettingScreen extends State<SettingScreen> {
-  List<String> imagePaths = [
-    'image/Pacute.jpg',
-    'image/Yasmin.jpg',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('image/Wallpaper_4.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Scaffold(
-          backgroundColor: Colors.white10,
-          appBar: AppBar(
-            centerTitle: true,
-            flexibleSpace: Container(
-                decoration: const BoxDecoration(
-              color: Colors.red,
-            )),
-            title: const Text(
-              'Support the Developers by:',
-              style: TextStyle(
-                  fontFamily: 'Alata',
-                  color: Color.fromARGB(255, 28, 31, 63),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 19),
-            ),
-          ),
-          body: Swiper(
-            itemBuilder: (BuildContext context, int index) {
-              return AccountScreen(imagePath: imagePaths[index]);
-            },
-            itemCount: 2,
-            layout: SwiperLayout.STACK,
-            itemWidth: MediaQuery.of(context).size.width * 0.8,
-            itemHeight: MediaQuery.of(context).size.height * 0.7,
-            pagination: const SwiperPagination(),
-            control: const SwiperControl(),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class AccountScreen extends StatelessWidget {
-  final String imagePath;
-  const AccountScreen({Key? key, required this.imagePath}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Column(
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: const Color.fromARGB(255, 28, 31, 63),
-                  radius: 120,
-                  child: CircleAvatar(
-                      backgroundColor: Colors.red,
-                      radius: 115,
-                      child: Image.asset(
-                        imagePath,
-                      )),
-                ),
-                const Text(
-                  'BASIC',
-                  style: TextStyle(
-                    fontSize: 35.0,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                print('Button pressed!');
-              },
-              child: const Text('Press me'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
